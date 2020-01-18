e9d3da4656f24d94a059de0c94a69080f546d648
夏先鹏 <951256916@qq.com>
2020-01-18 17:03:50 +0800

    配置

---
+++ src/main/java/com/example/springCache/entity/User.java
@@ -0,0 +1,101 @@
+package com.example.springCache.entity;
+
+import com.baomidou.mybatisplus.annotation.SqlCondition;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.extension.activerecord.Model;
+import java.time.LocalDateTime;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-08-25 01:35
+ */
+@TableName("user")
+public class User extends Model<User> {
+
+    private static final long serialVersionUID = 1L;
+    //@TableId(value = "id")
+    /*
+    自增策略
+    @TableId(type = IdType.AUTO)
+    */
+
+    /*
+    全局策略
+    @TableId(type = IdType.NONE)
+    */
+
+    /*
+    uuid
+    @TableId(type = IdType.UUID)
+    */
+
+
+    private Long id;
+    //@TableField("name")
+    @TableField(condition = SqlCondition.LIKE)
+    private String name;
+    private Integer age;
+    private String email;
+    private Long managerId;
+    private LocalDateTime createTime;
+
+    @TableField(exist = false)
+    private String remark;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public LocalDateTime getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(LocalDateTime createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+}
--- src/test/java/com/example/springCache/SpringCacheApplicationTests.java
+++ src/test/java/com/example/springCache/SpringCacheApplicationTests.java
@@ -1,11 +1,15 @@
-package com.example.springcache;
+package com.example.springCache;
 
+import com.example.springCache.mapper.UserMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringCacheApplicationTests {
 
+	@Autowired
+	UserMapper userMapper;
 	@Test
 	void contextLoads() {
 	}
